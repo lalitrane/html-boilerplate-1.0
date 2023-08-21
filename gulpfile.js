6fc853b23420b5b783bfc813f0889e462b77c972
Lalit <lalit@Lalits-MacBook-Pro.local>
2023-08-21 17:49:42 +1200

    Gulp automations added

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,110 @@
-const { src, dest, watch, series } = require('gulp');
-
+// Module Declaration
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const prefix = require('gulp-autoprefixer');
 const minify = require('gulp-clean-css');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
+const fileinclude = require('gulp-file-include');
+const server = require('browser-sync').create();
+const { watch, series } = require('gulp');
 
+// Path  Declaration
+const paths = {
+    root: {
+        src: './',
+        dest: './build/'
+    },
+    css: {
+        dest: './build/css'
+    },
+    scripts: {
+        dest: './build/js'
+    },
+    assets: {
+        dest: './build/assets'
+    }
+};
 
+// Reload Server
+async function reload() {
+    server.reload();
+}
 
-function compilecss() {
-
-    return src('scss/*.scss')
+// Compilecss, Minify and move to build Folder
+async function compileCss() {
+    gulp.src(['src/scss/*.scss'])
         .pipe(sass())
         .pipe(prefix())
         .pipe(minify())
-        .pipe(dest('css'))
+        .pipe(gulp.dest(paths.css.dest));
 }
+exports.compileCss = compileCss;
 
-
-
-// function movejs(){
-//     return src(['node_modules/jquery/dist/jquery.min.js','node_modules/bootstrap/dist/js/bootstrap.min.js'])
-//     .pipe(dest('dist/js'))
-// }
-
-function dependenciesjs() {
-    return src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/tiny-slider/dist/min/tiny-slider.js', 'js/main-dev.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/aos/dist/aos.js'])
+// Add dependencies, then concat into one JS, minify and move to build Folder
+async function addDependencies() {
+    gulp.src(['node_modules/jquery/dist/jquery.min.js',
+        'node_modules/tiny-slider/dist/min/tiny-slider.js',
+        'src/js/working.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/aos/dist/aos.js'])
         .pipe(concat('main.js'))
-        .pipe(dest('js/nonminified'));
-
-
-}
-
-function jsmin() {
-    return src('js/nonminified/*.js')
         .pipe(terser())
-        .pipe(dest('javascript'))
+        .pipe(gulp.dest(paths.scripts.dest));
 }
 
+exports.addDependencies = addDependencies;
 
-
-
-
-// function copyfonts(){
-
-//     return src('src/fonts/*') 
-//     .pipe(dest('dist/fonts'))
-// }
-
-
-
-function watchTask() {
-    watch('scss/*.scss', series(compilecss));
-    watch(['node_modules/jquery/dist/jquery.min.js', 'node_modules/tiny-slider/dist/min/tiny-slider.js', 'js/main-dev.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/aos/dist/aos.js'], dependenciesjs);
-    watch('js/nonminified/*.js', jsmin);
-    //watch('src/images/*.{jpg,png}',optimizeimg);
+// Copy assets to build folder
+async function copyAssets() {
+    gulp.src(['src/assets/**/*'])
+        .pipe(gulp.dest(paths.assets.dest));
 }
 
+exports.copyAssets = copyAssets;
+
+// Build files html and reload server
+async function buildAndReload() {
+    await includeHTML();
+    await compileCss();
+    await addDependencies();
+    await copyAssets();
+    reload();
+}
+
+async function includeHTML() {
+    return gulp.src([
+        'src/html/**/*.html',
+        '!src/html/header.html', // ignore
+        '!src/html/footer.html' // ignore
+    ])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest(paths.root.dest));
+}
+exports.includeHTML = includeHTML;
+
+exports.default = async function () {
+    // Serve files from the build folder
+    server.init({
+        server: {
+            baseDir: paths.root.dest
+        }
+    });
 
 
+    // Build and reload at the first time
+    buildAndReload();
 
-exports.default = series(
-    compilecss,
-    dependenciesjs,
-    jsmin,
-    watchTask
-    //optimizeimg,
-    //movejs,
-
-
-)
+    // Watch task
+    watch(["src/html/**/*",
+        "assets/**/*",
+        "src/scss/*.scss",
+        "src/js/*.js",
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/tiny-slider/dist/min/tiny-slider.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/aos/dist/aos.js'], series(buildAndReload));
+};
